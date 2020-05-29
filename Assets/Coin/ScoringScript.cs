74f0ee7e2f35724f23098f503e8044491a96ae2d
Martin Georgievski <martin.georgievski24@gmail.com>
2020-05-29 22:40:36 +0200

    Lab5

---
+++ Assets/Coin/ScoringScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoringScript : MonoBehaviour {
+    public int points = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnGUI()
+    {
+        //GUI.Label(new Rect(10, 10, 100, 20), "Score : " + points);
+
+        GUIStyle colorRed = new GUIStyle();
+
+        colorRed.normal.textColor = Color.red;
+        colorRed.fontSize = 30;
+
+        GUI.Label(new Rect(10, 10, 100, 20), "Score : " + points, colorRed);
+    }
+}
